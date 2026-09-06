caf15ff42f4a893a00f77d6f0bc64d984d4f0b16
Sayam Sarkar <sayamsarkar@Sayams-MacBook-Air.local>
2026-09-06 22:48:12 +0530

    url shortener service pre deployment setup

--- rate-limiter-url-shortener-service/src/main/java/com/sayam/rate_limiter_url_shortener_service/config/RedisConfig.java
+++ rate-limiter-url-shortener-service/src/main/java/com/sayam/rate_limiter_url_shortener_service/config/RedisConfig.java
@@ -2,31 +2,20 @@ package com.sayam.rate_limiter_url_shortener_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config =
-                new RedisStandaloneConfiguration("localhost", 6379);
-
-        config.setDatabase(0);
-
-        return new LettuceConnectionFactory(config);
-    }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
         RedisTemplate<String, String> template = new RedisTemplate<>();
 
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
